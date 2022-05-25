2eba13b1a4dfc8ad28ca011d9c96484a26dc1762
Justice-Tei21 <jusnwa050826@student.jenseneducation.se>
2022-05-25 14:29:42 +0200

    a more polished version
    still have to make the enemy bullet sprite and add some music.
    
    must not forget commenting.

--- classes.py
+++ classes.py
@@ -4,7 +4,8 @@ import time
 import pygame
 
 import weapon
-from main import *
+from images import asset_finder
+from main import WIDTH,HEIGHT, player_hit,enemy_hit, new_enemy_bullet
 
 
 import json
@@ -26,33 +27,34 @@ class Entities:  # base class for all entities/ only used for the player
 
 
 class Hostiles:
-    def __init__(self, screene, x, y, vel, image, hp, special):# lägg till bullet list
-        #super().__init__()
-        self.screen = screene
+    def __init__(self, screen, x, y, vel, image, hp, special, bullet_con): #  lägg till bullet list
+        # super().__init__()
+        self.screen = screen
         self.x = x
         self.y = y*32
         self.HP = hp
-        self.animation_count=0
+        self.animation_count = 0
         self.vel = vel[0]
         self.image = asset_finder(image).convert_alpha()
-        self.time_of_launch = random.randint(100,800)
-        self.new_vel = random.randint(vel[1][0],vel[1][1])
+        self.time_of_launch = random.randint(100, 800)
+        self.new_vel = random.randint(vel[1][0], vel[1][1])
+        self.bullet_con = bullet_con
 
 
-        self.special = special # the special feature of the class
-        actions={"shoot": self.shot_everything, "target": self.target,"march":self.marching_on}
+        self.special = special  # the state of the class
+        actions = {"shoot": self.shot_everything, "target": self.target, "march": self.marching_on}
         self.special_action = actions[special]
 
-        self.rect = self.image.get_rect(midleft=(self.x,self.y))#pygame.Rect(self.x, self.y, self.width, self.height)
+        self.rect = self.image.get_rect(midleft=(self.x, self.y))  # pygame.Rect(self.x, self.y, self.width, self.height)
         self.targeting = False
-        self.vector = pygame.math.Vector2(self.rect.centerx, self.rect.centery)
+        #self.vector = pygame.math.Vector2(self.rect.centerx, self.rect.centery)
 
 
         # prototype approaches
         #self.move_rect= [0,2]
 
     def draw(self):
-                # mix mellan draw och update metoderna
+        # mix mellan draw och update metoderna
         #self.rect.move(int(self.vector.x),int(self.vector.y))
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
@@ -96,11 +98,17 @@ class Hostiles:
     def shot_everything(self):
         curr_time = time.time()
         if curr_time - self.timer > 2:
+
             self.timer = curr_time
-            self.image = STATIC_IMAGE
+            image = pygame.Surface((50,50))
+            image.fill((255,0,0))
+            self.bullet_con.addbullet(self.rect.topleft)
+            #print(len(self.bullet_con))
 
     def marching_on(self):
         self.x-=self.vel
+
+
 
 class Hero(Entities):
     def __init__(self,*args):
@@ -110,13 +118,13 @@ class Hero(Entities):
         keys_pressed = pygame.key.get_pressed()
 
         if keys_pressed[pygame.K_d] and self.x + self.width < WIDTH:  # höger
-            self.x = self.x + VEL
+            self.x = self.x + self.vel
         if keys_pressed[pygame.K_w] and self.y > 0:  # upp
-            self.y = self.y - VEL
+            self.y = self.y - self.vel
         if keys_pressed[pygame.K_a] and self.x > 0:  # vänster
-            self.x = self.x - VEL
+            self.x = self.x - self.vel
         if keys_pressed[pygame.K_s] and self.y + self.height < HEIGHT:  # ner
-            self.y = self.y + VEL
+            self.y = self.y + self.vel
 
         self.rect.x = self.x
         self.rect.y = self.y
@@ -132,10 +140,8 @@ class Hero(Entities):
     def update(self):
         self.player_movement()
 
-
-
-class enemyManager:
-    def __init__(self, screen, player, bullets, clock, enemy_types):
+class EnemyManager:
+    def __init__(self, screen, player, bullets, clock, enemy_types, bullet_list):
         self.enemies = []
         self.screen = screen
         self.player = player
@@ -145,32 +151,32 @@ class enemyManager:
         self.enemies_killed = 0
         self.enemy_types = enemy_types
         self.active = True
-
+        self.bullet_con = bullet_list
 
         with open("enemies.json", "r") as enemy_list:
-            self.enemy_attribs= []
-            enemy_json_file=json.load(enemy_list)
+            self.enemy_attribs = []
+            enemy_json_file = json.load(enemy_list)
             for ship in enemy_types:
-                #print(self.enemy_types)
-                #print(ship)
-                aa=enemy_json_file[str(ship[0])]
-                speeds= aa["movement"]
-                sprite= aa["sprite"]
-                hp= aa["health"]
+                # print(self.enemy_types)
+                # print(ship)
+                aa = enemy_json_file[str(ship[0])]
+                speeds = aa["movement"]
+                sprite = aa["sprite"]
+                hp = aa["health"]
                 special = aa["special"]
-                #print(ship[1])
-                aa= Hostiles(screen,WIDTH+60,random.randint(1,17),speeds,sprite,hp,special)
-                self.enemy_attribs.append([speeds,sprite,hp,ship[1],special])
+                # print(ship[1])
+                # aa = Hostiles(screen, WIDTH+60, random.randint(1, 17), speeds, sprite, hp, special)
+                self.enemy_attribs.append([speeds, sprite, hp, ship[1], special])
 
-
-    # creates an enemy of a certain type and adds it to the current list
-    def addEnemy(self):
+    #  creates an enemy of a certain type and adds it to the current list
+    def addenemy(self):
 
         for item in self.enemy_attribs:  # maybe use enemy_type
             # print(item)
 
-            if self.clock % item[3] == 0: # use [1]
-                an_enemy = Hostiles(screen, WIDTH, random.randint(1, 17), item[0], item[1], item[2], item[4])
+            if self.clock % item[3] == 0:  # use [1]
+                an_enemy = Hostiles(self.screen, WIDTH, random.randint(1, 17), item[0], item[1],
+                                    item[2], item[4], self.bullet_con)
 
                 self.enemies.append(an_enemy)
 
@@ -196,49 +202,62 @@ class enemyManager:
                 for bullet in self.bullets:
                     if bullet.rect.colliderect(enemy.rect):
                         enemy.HP -= self.player.damage
-                        self.killEnemy(enemy)
+                        self.killenemy(enemy)
                         self.bullets.remove(bullet)
 
-    def killEnemy(self, enemy):
+    def killenemy(self, enemy):
         if enemy.HP < 1:
             self.enemies.remove(enemy)
             self.enemies_killed += 1
 
     def update(self, timer):
         if self.active:
-            self.addEnemy()
+            self.addenemy()
             self.move()
-            self.inTimer(timer)
+            self.intimer(timer)
         self.enemy_collisions()
 
-    def inTimer(self, timer):
+    def intimer(self, timer):
         self.clock += 1
         self.clock %= 3600
-
-
-
 
 
 class Bullet(Entities):
 
     def move(self):
-        self.x+=self.vel
+        self.x += self.vel
+
 
 class BulletHandler:
-    def __init__(self):
+    def __init__(self, screen):
         self.enemybullets = []
         self.player_bullets = []
+        self.screen = screen
+        self.image = asset_finder("static.png").convert_alpha()
 
-    def addBullet(self, weapons_list):
-        self.enemybullets += weapons_list
+    def addbullet(self, position):
+        self.enemybullets.append(self.image.get_rect(topleft=(position)))
 
-    def collision(self,player):
+    def collision(self,player,bullet):
+        #for bullet in self.enemybullets:
+
+        if bullet.colliderect(player.rect):
+            pygame.event.post(pygame.event.Event(player_hit))
+            self.enemybullets.remove(bullet)
+
+        if bullet.x + bullet.width >= WIDTH or bullet.x < 0:
+            self.enemybullets.remove(bullet)
+
+    @staticmethod
+    def move(bullet):
+        bullet.x -= 6
+
+    def draw(self):
         for bullet in self.enemybullets:
-            if bullet.rect.colliderect(player.rect):
-                player.HP -= bullet.damage
+            self.screen.blit(self.image, bullet.topleft)
 
-            if bullet.rect.x + bullet.width >= WIDTH or bullet.rect.x < 0:
-                self.enemybullets.remove(bullet)
+    def update(self, player):
+        for bullet in self.enemybullets:
+            self.move(bullet)
+            self.collision(player, bullet)
 
-    def update(self):
-        pass
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 from classes import *
 import random
 import time
-from images import *
 
 
+import math
 pygame.init()
 pygame.font.init()
 
@@ -17,14 +17,13 @@ WHITE = (255, 255, 0)
 VEL = 5
 BULLET_VEL = 12
 MAX_BULLETS = 5
-
+x=0
 PLAYER_WIDTH, PLAYER_HEIGHT = 64, 64
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT),pygame.SCALED)
 
 
-pygame.display.set_caption('Smoke')
-pygame.display.set_icon(icon)
+
 
 
 player_hit = pygame.USEREVENT + 1
@@ -32,40 +31,55 @@ enemy_hit = pygame.USEREVENT + 2
 new_enemy_bullet = pygame.USEREVENT +3
 
 
-def drawcontainers(image, placementy, placementx, maximum=0, objects=[]):
+def drawcontainers(i, placementy, placementx, maximum=0, objects=[]):
     offset = 0
     for amount in range(int(maximum-len(objects))):
-        screen.blit(image, (offset*32+placementx, placementy))
+        screen.blit(i, (offset*32+placementx, placementy))
         offset += 1
 
 
-def draw_window(green, player_bullet, enemies, current_health, power_up,clock):
+def draw_window(green, player_bullet, enemies, current_health, power_up,clock,bullet_con):
     #screen.fill((0, 0, 0, 1))
-    screen.blit(background, (0, 0))
+    global background_pos
+    background_mover(background_list, background_pos)
+    #screen.blit(background, (0, 0))
 
     for bullet in player_bullet:
         screen.blit(bullet_image, bullet)
         if power_up:
             pygame.draw.circle(screen, (0, 255, 255), bullet.rect.center, 20.0, 4)
+    bullet_con.draw()
     enemies.draw()
     green.draw()
+
 
     drawcontainers(bullet_image, 50, 10, MAX_BULLETS, player_bullet)
     drawcontainers(heart_image, 20, 10, current_health)
     a=pygame.font.Font(pygame.font.get_default_font(),20).render(str(int(clock.get_fps())),False,(0,0,0))
     screen.blit(a,(300,0))
+    #global x
+    #x+=0.1
+    #pygame.draw.arc(screen,(0,0,255),screen.get_rect(topleft=(0,0)),math.fabs(math.sin(x)*20),40)
+
     pygame.display.update()
 
-bullet_image.convert_alpha()
 def handle_bullets(player_bullets, green):
 
-    # This function is redundant since it is now handled by a class
+    # /This function is redundant since it is now handled by a class
     # not actually redundant yet since i'm making the class
     for bullet in player_bullets:
         bullet.rect.x += bullet.vel
 
         if bullet.rect.x + bullet.width >= WIDTH or bullet.rect.x < 0:
             player_bullets.remove(bullet)
+
+def background_mover(background_lis,pos):
+    for i, value in enumerate(background_lis,0):
+        screen.blit(value,(pos[i],0))
+        pos[i]-=10
+        if pos[i] <= -WIDTH:
+            pos[i] = WIDTH
+
 
 
 def scene(difficulty, health=10, enemies_to_kill=20, playerpos=(200, 300)):
@@ -83,21 +97,22 @@ def scene(difficulty, health=10, enemies_to_kill=20, playerpos=(200, 300)):
     player_health = health
     clock = pygame.time.Clock()
     power_up = False
-    RUNNING = True
+    running = True
     playing = True
-    enemyhandler = enemyManager(screen, green, player_bullets, timer, difficulty)
-    e_BulletHandler= BulletHandler()
+    e_BulletHandler = BulletHandler(screen)
+    enemyhandler = EnemyManager(screen, green, player_bullets, timer, difficulty, e_BulletHandler)
     active = True
-    while RUNNING:
-        timer +=1
-        timer = timer%3600
-        clock.tick(FPS)
 
+
+    while running:
+        timer += 1
+        timer = timer % 3600
+        clock.tick(FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                RUNNING = False
-                playing= False
+                running = False
+                playing = False
 
             if event.type == pygame.WINDOWFOCUSLOST:
                 active = False
@@ -121,17 +136,19 @@ def scene(difficulty, health=10, enemies_to_kill=20, playerpos=(200, 300)):
             if event.type == player_hit:
                 player_health -=1
                 if player_health<=0:
-                    RUNNING=False
+                    running=False
                     pygame.quit()
         if active:
+
             green.update()
             handle_bullets(player_bullets, green)
-            draw_window(green, player_bullets, enemyhandler, player_health, power_up, clock)
+            draw_window(green, player_bullets, enemyhandler, player_health, power_up, clock,e_BulletHandler,)
             enemyhandler.update(timer)
+            e_BulletHandler.update(green)
             if enemyhandler.enemies_killed > enemies_to_kill:
                 screen.blit(asset_finder("win screen.png").convert_alpha(), (200, 200))
                 pygame.display.update()
-                RUNNING = False
+                running = False
 
     return player_health,enemyhandler.enemies_killed, playing,(green.x,green.y)
 
@@ -159,11 +176,10 @@ def intro_screen():
         else:
             HEIGHT=608
 
+        background = pygame.Surface((WIDTH,HEIGHT))
 
-        Background=pygame.Surface((WIDTH,HEIGHT))
-
-        Background.blit(Title, (0, 0))
-        screen.blit(Background,(0, 0))
+        background.blit(Title, (0, 0))
+        screen.blit(background,(0, 0))
 
 
         key_pressed=pygame.key.get_pressed()
@@ -205,5 +221,18 @@ def main():
 
 
 if __name__ == '__main__':
-    print(pygame.font.get_fonts())
+    from images import *
+
+    pygame.display.set_caption('Smoke')
+    pygame.display.set_icon(icon)
+
+    background.convert_alpha()
+    background_list = [background, background.copy()]
+    background_pos = [0, WIDTH]
+
+
+
+    bullet_image.convert_alpha()
+
+    #print(pygame.font.get_fonts())
     main()
